e96a9a848245efb8bb575b002597023407258b75
reda bensaltana <73000726+redabensaltana@users.noreply.github.com>
2021-11-24 21:12:46 +0100

    last version

--- main.c
+++ main.c
@@ -2,23 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
-int option,nombre_de_comptes_a_ajouter;
+int option;
+int nombre_de_comptes_a_ajouter;
 int compteur_de_comptes = 0;
-char comptes_bancaires_noms[100][30];
+int index_cin = 0;
 char comptes_bancaires_cin[100][30];
+char comptes_bancaires_noms[100][30];
 int comptes_bancaires_montants[100];
 float pourcentage_a_ajouter[3];
 char tmp[30];
-int index_cin = 0;
 int montant_retrait_depot = 0;
 int trouve = 0;
 
-void space(){
-    printf("\n\n\n\n\n");
+void separer(){
+    printf("\n\n\n\n\n\n\n\n");
 }
 
 void menu_principal(){
-    space();
+    separer();
     printf("===================================| MENU PRINCIPAL |================================\n\n\n");
     printf("\t1.Introduire un compte bancaire (CIN, Nom et Prenom, montant).\n");
     printf("\t2.Introduire plusieurs comptes bancaires (CIN, Nom et Prenom, Montant).\n");
@@ -39,9 +40,9 @@ void entree_compte(){
 
 }
 int recherche_par_cin(){
-        char recherche_cin[30];
-        int index_cin_fonction;
-        do{
+    char recherche_cin[30];
+    int index_cin_fonction;
+    do{
         printf("Veuillez entrer CIN du client : ");
         scanf("%s", &recherche_cin);
         for (int i = 0; i < compteur_de_comptes; i++) {
@@ -54,18 +55,17 @@ int recherche_par_cin(){
                 index_cin_fonction = i;
 
                 break;
-        }
+            }
 
-    }
+        }
         if(trouve == 0){
             printf("Votre cin non trouve\n");
         }
-        }while(trouve == 0);
-        trouve = 0;
-        return index_cin_fonction;
+    }while(trouve == 0);
+    return index_cin_fonction;
 }
 void affichage_menu(){
-    space();
+    separer();
     printf("=========================================|affichage|=================================================");
     printf("\n1-Par Ordre Ascendant\n");
     printf("2-Par Ordre Descendant\n");
@@ -157,16 +157,15 @@ void tri_descendent(){
     }
 }
 void affichage_tableau(int debut,int fin){
-    space();
-    for (int i = debut ; i < fin; i++) {
-        printf("%d\n", comptes_bancaires_montants[i]);
 
-        printf("%s\n", comptes_bancaires_cin[i]);
-
-        printf("%s\n", comptes_bancaires_noms[i]);
+        printf("=======================================================================\n");
+        for(int i=debut;i<fin;i++){
+            printf("|\t%10s\t|\t%10s\t|\t%8d\t|\n",comptes_bancaires_noms[i], comptes_bancaires_cin[i], comptes_bancaires_montants[i]);
+        }
+        printf("=======================================================================");
     }
 
-}
+
 
 
 
@@ -181,26 +180,27 @@ int main() {
         switch (option) {
             //Introduire un compte bancaire (CIN, Nom et Prénom, Montant)
             case 1:{
-                space();
+                separer();
                 printf("=====================|entree de comptes|=======================");
                 entree_compte();
                 break;
             }
-            //Introduire plusieurs comptes bancaires (CIN, Nom et Prénom, Montant)
+                //Introduire plusieurs comptes bancaires (CIN, Nom et Prénom, Montant)
             case 2:{
-                space();
-                printf("=====================|entree de comptes|=======================\n\n");
+                separer();
+                printf("=============================|entree de comptes|=============================\n\n");
                 printf("Saisissez le nombre de comptes que vous souhaitez : ");
                 scanf("%d", &nombre_de_comptes_a_ajouter);
                 for (int i = 0; i < nombre_de_comptes_a_ajouter; i++) {
                     entree_compte();
+                    printf("\n__________________________________________________________________________________________\n");
                 }
                 break;}
-            //Opérations
+                //Opérations
             case 3:{
-                space();
+                separer();
                 printf("=====================|operations|=======================");
-                printf("1-Retrait.\n 2-Depot.\n");
+                printf("\n1-Retrait.\n2-Depot.\n");
                 printf("Veuillez choisir votre option :");
                 scanf("%d", &option);
 
@@ -215,9 +215,9 @@ int main() {
                             printf("Le nouveau montant est %d\n", comptes_bancaires_montants[index_cin]);
                             trouve = 0;
                         }
-                            break;
+                        break;
                     }
-                    //Dépôt
+                        //Dépôt
                     case 2 : {
 
                         index_cin = recherche_par_cin();
@@ -228,12 +228,12 @@ int main() {
                             printf("Le nouveau montant est %d\n", comptes_bancaires_montants[index_cin]);
                             trouve = 0;
                         }
-                                break;
+                        break;
                     }
                 }
                 break;
             }
-            //Affichage
+                //Affichage
             case 4:{
                 affichage_menu();
                 printf("\n Veuillez choisir votre option : ");
@@ -243,101 +243,98 @@ int main() {
                     //Par Ordre Ascendant
                     case 1 : {
 
-                       tri_ascendent();
+                        tri_ascendent();
                         affichage_tableau(0,compteur_de_comptes);
                         break;
                     }
-                    //Par Ordre Descendant
+                        //Par Ordre Descendant
                     case 2 : {
                         tri_descendent();
                         affichage_tableau(0,compteur_de_comptes);
                         break;
                     }
-                    //Par Ordre Ascendant (les comptes bancaires ayant un montant supérieur à un chiffre introduit)
+                        //Par Ordre Ascendant (les comptes bancaires ayant un montant supérieur à un chiffre introduit)
                     case 3 : {
                         int recherche_de_montant;
                         int index_recherche_de_montant;
                         tri_ascendent();
                         do{
-                        printf("S il vous plait entrez le montant que vous souhaitez rechercher:");
-                        scanf("%d", &recherche_de_montant);
+                            printf("S il vous plait entrez le montant que vous souhaitez rechercher:");
+                            scanf("%d", &recherche_de_montant);
 
-
-                        for (int i = 0; i < compteur_de_comptes; i++) {
-                            if (comptes_bancaires_montants[i] == recherche_de_montant) {
-                                index_recherche_de_montant = i;
-                                trouve = 1;
-                                break;
+                            for (int i = 0; i < compteur_de_comptes; i++) {
+                                if (comptes_bancaires_montants[i] == recherche_de_montant) {
+                                    index_recherche_de_montant = i;
+                                    trouve = 1;
+                                    break;
                                 }
-                             }
-                        if(trouve == 0){
-                            printf("Le montant saisie n existe pas, veuillez reinserer le montant : ");
+                            }
+                            if(trouve == 0){
+                                printf("Le montant saisie n existe pas !!\n");
+                            }
+                            else{
+                                affichage_tableau(index_recherche_de_montant,compteur_de_comptes);
+                            }
                         }
-                        else{
-                            affichage_tableau(0,index_recherche_de_montant);
-                        }
-                    }
                         while(trouve == 0);
                         trouve = 0;
 
                         break;
                     }
-                    //Par Ordre Descendant (les comptes bancaires ayant un montant supérieur à un chiffre introduit)
+                        //Par Ordre Descendant (les comptes bancaires ayant un montant supérieur à un chiffre introduit)
                     case 4 : {
                         int recherche_de_montant;
                         int index_recherche_de_montant;
                         tri_descendent();
                         do{
 
-                        printf("S il vous plait entrez le montant que vous souhaitez rechercher:");
-                        scanf("%d", &recherche_de_montant);
+                            printf("S il vous plait entrez le montant que vous souhaitez rechercher:");
+                            scanf("%d", &recherche_de_montant);
 
 
-                        for (int i = 0; i < compteur_de_comptes; i++) {
-                            if (comptes_bancaires_montants[i] == recherche_de_montant) {
-                                index_recherche_de_montant = i;
-                                trouve = 1;
-                                break;
+                            for (int i = 0; i < compteur_de_comptes; i++) {
+                                if (comptes_bancaires_montants[i] == recherche_de_montant) {
+                                    index_recherche_de_montant = i;
+                                    trouve = 1;
+                                    break;
+                                }
                             }
+                            if(trouve == 0){
+                                printf("Le montant saisie n existe pas !! ");
+                            }
+                            else
+                                affichage_tableau(0,index_recherche_de_montant);
                         }
-                        if(trouve == 0){
-                        printf("Le montant saisie n existe pas, veuillez reinserer le montant : ");
-                        }
-                        else
-                            affichage_tableau(0,index_recherche_de_montant);
-                        }
-                    while(trouve == 0);
-                    trouve = 0;
+                        while(trouve == 0);
+                        trouve = 0;
                         break;
 
                     }
-                    //Recherche par CIN
+                        //Recherche par CIN
                     case 5 : {
                         recherche_par_cin();
                         affichage_tableau(index_cin,index_cin+1);
-
+                        trouve = 0;
                         break;
                     }
 
                 }
                 break;
             }
-            //Fidélisation
+                //Fidélisation
             case 5:{
-                space();
+                separer();
                 printf("=====================|fidelisation|=======================");
                 tri_descendent();
 
                 for (int i = 0; i < 3 ; i++) {
 
                     pourcentage_a_ajouter[i] = (comptes_bancaires_montants[i] * 1.3) / 100;
+                    comptes_bancaires_montants [i]  += pourcentage_a_ajouter[i];
 
-                    printf("%d\n", comptes_bancaires_montants [i]  += pourcentage_a_ajouter[i]);
 
-                    printf("%s\n", comptes_bancaires_cin[i]);
-
-                    printf("%s\n", comptes_bancaires_noms[i]);
                 }
+                affichage_tableau(0,3);
 
                 break;
             }
@@ -349,5 +346,3 @@ int main() {
 
     return 0;
 }
-
-
